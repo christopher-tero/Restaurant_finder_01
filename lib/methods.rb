cea9e0225888811f651b4990f7f59c5a44c7a8c0
Christopher Tero <christopher.tero@gmail.com>
2019-05-09 09:25:46 -0600

    combining code for better functionality

--- lib/methods.rb
+++ lib/methods.rb
@@ -34,25 +34,23 @@ def user_list
   end
 end
 
-#/// Select user: ///
+# /// Select user: ///
 
 def select_user
   puts "\n\n".white_on_blue
   print "Please enter your name, type 'new user', or exit.".center(85).white_on_blue
   puts "\n\n".white_on_blue
-  selection = gets.chomp.downcase
+  name = gets.chomp.capitalize
+  puts ""
   #binding.pry
-  if user_list.include?(selection)
-    valid_user = selection
-  end
-  case selection
-  when valid_user
+  if User.exists?(name: name)
     puts "\n".white_on_magenta
-    print "Hello #{selection}! How can we help you today?".center(85).white_on_magenta
+    print "Hello #{name}! How can we help you today?".center(85).white_on_magenta
     puts "\n".white_on_magenta
-  when "new user"
+    #main_menu
+  elsif name.downcase == "new user"
     new_user
-  when "exit"
+  elsif name.downcase == "exit"
     exit
   else
     puts "\n".center(85).white_on_red
@@ -62,29 +60,18 @@ def select_user
   end
 end
 
-def selected_user
-
-end
-
-# def find_user(selection)
-#   User.all.find do |person|
-#     #binding.pry
-#     person.name == selection
-#   end
-# end
-
 def new_user
     puts "Welcome to our app! Please enter your name:".cyan
-    new_name = gets.chomp
-    if user_list.include?(new_name)
+    name = gets.chomp.capitalize
+    puts ""
+    if User.exists?(name: name)
       puts "Sorry! This User name is already taken. Please try again"
       new_user
     else
-      puts "\nHello #{new_name}!"
-      User.create(name: new_name)
+      puts "\nHello #{name}!"
+      User.create(name: name)
     end
 end
-
 
 def main_menu
   puts "\n"
@@ -132,7 +119,6 @@ def top_10
   puts "\n"
 
   best_rest_name = top_restaurant_name.map do |restaurant|
-    # binding.pry
     if restaurant.location.downcase == $location
       restaurant.name
     end
@@ -140,15 +126,16 @@ def top_10
   puts best_rest_name.compact.last(10)
   puts "\n"
   puts "\n\n".white_on_blue
+
 end
 
 
 def cuisine
   puts "You selected cuisine; please select one of the following numbers:".center(85).green
   puts "1 - American".center(85).green
-  puts "2 - Mexican".center(85).green
+  puts "2 - Mexican ".center(85).green
   puts "3 - Japanese".center(85).green
-  puts "4 - Italian".center(85).green
+  puts "4 - Italian ".center(85).green
   cuisine_choice = gets.chomp.to_i
   if cuisine_choice == 1
     cuisine_selection = "American"
@@ -163,23 +150,24 @@ def cuisine
   cuisine_random = Restaurants.all.select do |restaurant|
     restaurant.name if restaurant.cuisine == cuisine_selection
   end ##### This iterator should work for all below cases #####
+
+  rando_cuisine = cuisine_random.sample
   case cuisine_choice.to_i
-  when 1
+    when 1
       puts "Random American restaurant: ".center(85).cyan
-      puts "#{cuisine_random.sample.name}".center(85).cyan
+      puts "name: #{rando_cuisine.name}, price: #{rando_cuisine.price}, rating: #{rando_cuisine.rating}".center(85).cyan
       # Add a save function
     when 2
       puts "Random Mexican restaurant: ".center(85).cyan
-      puts "#{cuisine_random.sample.name}".center(85).cyan
+      puts "name: #{rando_cuisine.name}, price: #{rando_cuisine.price}, rating: #{rando_cuisine.rating}".center(85).cyan
       # Add a save function
     when 3
       puts "Random Japanese restaurant: ".center(85).cyan
-      puts "#{cuisine_random.sample.name}".center(85).cyan
+      puts "name: #{rando_cuisine.name}, price: #{rando_cuisine.price}, rating: #{rando_cuisine.rating}".center(85).cyan
       # Add a save function
     when 4
       puts "Random Italian restaurant: ".center(85).cyan
-      puts "#{cuisine_random.sample.name}".center(85).cyan
-      # Add a save function
+      puts "name: #{rando_cuisine.name}, price: #{rando_cuisine.price}, rating: #{rando_cuisine.rating}".center(85).cyan
     else
       puts "\n".center(85).white_on_red
       print "Please enter a valid selection".center(85).white_on_red
@@ -200,22 +188,22 @@ def price
   price_random = Restaurants.all.select do |restaurant|
     restaurant if restaurant.price.length == price_range.to_i
   end
-  $restaurant_selection = price_random.sample.name
+  restaurant_selection = price_random.sample
   case price_range
   when "1"
       print "You selected budget restaurants:".center(85).magenta
       puts "\n\n"
-      puts "#{$restaurant_selection}".center(85).cyan_on_red
+      puts "name: #{restaurant_selection.name}, rating: #{restaurant_selection.rating}".center(85).cyan_on_red
       puts "\n"
     when "2"
       print "You selected moderately priced restaurants:".center(85).magenta
       puts "\n\n"
-      puts "#{$restaurant_selection}".center(85).cyan_on_red
+      puts "name: #{restaurant_selection.name}, rating: #{restaurant_selection.rating}".center(85).cyan_on_red
       puts "\n"
     when "3"
       print "You selected expensive restaurants:".center(85).magenta
       puts "\n\n"
-      puts "#{$restaurant_selection}".center(85).cyan_on_red
+      puts "name: #{restaurant_selection.name}, rating: #{restaurant_selection.rating}".center(85).cyan_on_red
       puts "\n"
     else
       puts "\n".white_on_red
@@ -235,15 +223,18 @@ def rating ### Working for selection, but need to return string instead of item 
   case rating_selection
     when "3"
       puts "Three star restaurant:"
-      puts "#{stars_random.sample.name}"
+      rando = stars_random.sample
+      puts "name: #{rando.name}, price:#{rando.price}"
       # Add a save function
     when "4"
       puts "Four star restaurant:"
-      puts "#{stars_random.sample.name}"
+      rando = stars_random.sample
+      puts "name: #{rando.name}, price:#{rando.price}"
       # Add a save function
     when "5"
       puts "Five star restaurant:"
-      puts "#{stars_random.sample.name}"
+      rando = stars_random.sample
+      puts "name: #{rando.name}, price:#{rando.price}"
       # Add a save function
     else
       "Please enter a valid input"
@@ -259,7 +250,12 @@ def city_restaurants
   rando = Restaurants.all.select do |restaurant|
     restaurant.location.downcase == $location
   end
-  puts rando.sample.name
+  city_rando = rando.sample
+  puts "name: #{city_rando.name}, price: #{city_rando.price}, rating: #{city_rando.rating}"
+end
+
+def end_of_method
+  puts
 end
 
 
